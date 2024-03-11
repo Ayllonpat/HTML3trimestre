d4652129bb8e96685f7d76b98d32fdb6a5488d98
Patricia Alonso Ayllón <alonso.aypat22@triana.salesianos.edu>
2024-03-11 10:01:32 +0100

    gsa<g

---
+++ EjemplosTema8/JS/Ejemplo1.js
@@ -0,0 +1,4 @@
+alert("Hola Mundo"); //alert saca una pagina pequeña
+let mes = prompt("Indica un mes"); //prompt nos permite escribir
+///=== comprara el valor y el tipo de dato
+alert("Estamos es "+mes);

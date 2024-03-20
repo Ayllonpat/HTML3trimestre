2212d9189f92cfc408a3265705b076ab9f447495
Patricia Alonso Ayllón <alonso.aypat22@triana.salesianos.edu>
2024-03-20 11:59:30 +0100

    afñksfjk

---
+++ EjerciciosTema8/Ejercicio06/JS/Ejercicio6.js
@@ -0,0 +1,8 @@
+
+let letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D',
+'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E','T'];
+
+let dni=prompt("Indique los números de su dni:");
+let letra=prompt("Indique la letras de su dni:");
+
+letra.toUpperCase;
